87b97962551091ed0e15af50e2ad32b7714cbdc9
parapore <eriglai577@gmail.com>
2024-03-22 16:26:01 +0900

    add type definition.

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { sampleSlice } from "../reducer/reducer";
+import { useDispatch, useSelector } from 'react-redux'
+import type { TypedUseSelectorHook } from 'react-redux'
 
 
 //Store生成
@@ -10,4 +12,16 @@ const store = configureStore({
     }
 });
 
+// TypeScriptの場合は下記をやらずに素のuseSelectorを使うと「does not exist on type ~」エラー出る。
+// 参考：https://redux-toolkit.js.org/tutorials/typescript#define-typed-hooks
+// 型推論＆型エイリアス宣言。
+// RootStateという型エイリアスを宣言
+// store.getStateでstore自身からStateを取得して型推論。具体的にはsampleSliceのState型になる。
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+// カスタムフック定義＆型定義
+// 素の `useDispatch`をuseuseAppDispatchという名前にしてカスタムフック定義、戻り値の型をAppDispatchに定義。
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector //「const useAppSelector = useSelector」に型定義TypedUseSelectorHook<RootState>を加えているだけ
 export default store;
